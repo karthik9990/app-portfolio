57157132e3e237ce011cb6f23d0697680c098879
karthik9990 <karthik.tadakapalli@gmail.com>
2024-03-26 13:29:50 -0600

    Day 23 >>Send Emails the form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,4 +1,26 @@
 import streamlit as st
+from send_email import send_email
+
 
 st.header("Contact US:")
+
+with st.form(key="email_forms"):
+    user_email = st.text_input("Your Email Address:")
+    user_message = st.text_area("Message")
+    message = f"""\
+Subject: New Email from {user_email}
+
+From: {user_email}
+Message: {user_message}
+"""
+    submit_button = st.form_submit_button("Submit")
+
+    if submit_button:
+        send_email(message)
+        st.info("Your Email was sent successfully")
+
+
+
+
+
 
